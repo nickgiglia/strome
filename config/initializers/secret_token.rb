758de85cb406d460757b16427506e82cbb922643
U-NickGiglia\Nick <Nick@NickGiglia.(none)>
2011-08-11 16:50:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strome::Application.config.secret_token = 'f2be4802625ffe8da78a12e1488f3647f4166435f296d15666e5072e88485757187f1d00c56e9df988d39794c449d80828e4fa265f606e0694ef52d1ae1f0184'
